21b06e5c1eeedf60d2de5be926c6bc62ec408688
Jake Dexheimer <jakedex@gmail.com>
2016-04-15 01:54:53 -0500

    Remove bloat - custom msgs, dynamic reconfigure, etc

--- include/perlin_noise_node/talker.h
+++ include/perlin_noise_node/talker.h
@@ -5,16 +5,7 @@
 #include "ros/ros.h"
 #include "ros/time.h"
 
-// Custom message includes. Auto-generated from msg/ directory.
-#include "node_example/NodeExampleData.h"
-
-// Dynamic reconfigure includes.
-#include <dynamic_reconfigure/server.h>
-// Auto-generated from cfg/ directory.
-#include <node_example/nodeExampleConfig.h>
-
 // Filter includes
-#include "ros/ros.h"
 #include "perlin_noise_filter/perlin_noise_filter.h"
 #include "filters/filter_chain.h"
 
@@ -27,9 +18,6 @@ public:
   //! Constructor.
   ExampleTalker(ros::NodeHandle nh);
 
-  //! Callback function for dynamic reconfigure server.
-  void configCallback(node_example::nodeExampleConfig& config, uint32_t level);
-
   //! Timer callback for publishing message.
   void timerCallback(const ros::TimerEvent& event);
 
@@ -41,19 +29,7 @@ private:
   ros::Timer timer_;
 
   //! Message publisher.
-  ros::Publisher pub_;
-
-  //! Dynamic reconfigure server.
-  dynamic_reconfigure::Server<node_example::nodeExampleConfig> dr_srv_;
-
-  //! The actual message.
-  std::string message_;
-
-  //! The first integer to use in addition.
-  int a_;
-
-  //! The second integer to use in addition.
-  int b_;
+  // ros::Publisher pub_;
 };
 
 }
--- src/talker.cpp
+++ src/talker.cpp
@@ -4,17 +4,8 @@ namespace node_example
 {
 
 ExampleTalker::ExampleTalker(ros::NodeHandle nh) :
-    a_(1),
-    b_(2),
-    message_("hello"),
     filter_chain("double")
 {
-
-  // Set up a dynamic reconfigure server.
-  // Do this before parameter server, else some of the parameter server values can be overwritten.
-  dynamic_reconfigure::Server<node_example::nodeExampleConfig>::CallbackType cb;
-  cb = boost::bind(&ExampleTalker::configCallback, this, _1, _2);
-  dr_srv_.setCallback(cb);
 
   // Declare variables that can be modified by launch file or command line.
   int rate;
@@ -22,22 +13,15 @@ ExampleTalker::ExampleTalker(ros::NodeHandle nh) :
   // Initialize node parameters from launch file or command line. Use a private node handle so that multiple instances
   // of the node can be run simultaneously while using different parameters.
   ros::NodeHandle pnh("~");
-  pnh.param("a", a_, 99);
-  pnh.param("b", b_, b_);
-  pnh.param("message", message_, message_);
   pnh.param("rate", rate, 1);
 
   // Create a publisher and name the topic.
-  pub_ = nh.advertise<node_example::NodeExampleData>("example", 10);
+  // pub_ = nh.advertise<node_example::NodeExampleData>("example", 10);
 
   // Create timer.
   timer_ = nh.createTimer(ros::Duration(1 / rate), &ExampleTalker::timerCallback, this);
 
-  if(filter_chain.configure("perlin_params")) {
-    ROS_INFO("configured sucessfully");
-  } else {
-    ROS_INFO("configured unsucessfully");
-  }
+  filter_chain.configure("perlin_params");
 
   double filt_in = 0.9;
   double filt_out = 9;
@@ -63,23 +47,14 @@ void ExampleTalker::timerCallback(const ros::TimerEvent& event)
   //   ROS_INFO("update unsuccessful");
   // }
 
-  node_example::NodeExampleData msg;
-  msg.message = message_;
+  // node_example::NodeExampleData msg;
+  // msg.message = message_;
 
   // ROS_INFO("post update, result is: %f", result);
 
-  msg.a = 0;
-  msg.b = b_;
+  // msg.a = 0;
+  // msg.b = b_;
 
-  pub_.publish(msg);
+  // pub_.publish(msg);
 }
-
-void ExampleTalker::configCallback(node_example::nodeExampleConfig& config, uint32_t level)
-{
-  // Set class variables to new values. They should match what is input at the dynamic reconfigure GUI.
-  message_ = config.message.c_str();
-  a_ = config.a;
-  b_ = config.b;
-}
-
 }
